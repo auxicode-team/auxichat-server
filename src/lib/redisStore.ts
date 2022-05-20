8fa49550f96a6a3604e100a2320e78c61ecc5023
nazmuul420 <nazmuul420@gmail.com>
2022-05-20 19:29:16 +0600

    add ci-cd to heroku, fixed dynamic redis uri

--- src/lib/redisStore.ts
+++ src/lib/redisStore.ts
@@ -2,7 +2,11 @@ import * as session from "express-session";
 import * as Redis from "ioredis";
 import * as connectRedis from "connect-redis";
 
-const client = new Redis(process.env.REDIS_URI);
+const client = new Redis(
+  parseInt(process.env.REDIS_PORT!),
+  process.env.REDIS_HOSTNAME,
+  { password: process.env.REDIS_PASSWORD },
+);
 client.on("connect", () => {
   console.log("Connected to Redis!");
 });
